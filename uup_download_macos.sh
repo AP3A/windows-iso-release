eb6a4552c9cef33fba133be00648e42f00b7229c
Baklava <b@e.com>
2025-11-17 21:25:05 +0700

    23H2

--- uup_download_macos.sh
+++ uup_download_macos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Generated on 2025-11-17 12:43:30 GMT
+#Generated on 2025-11-17 14:20:59 GMT
 
 # Proxy configuration
 # If you need to configure a proxy to be able to connect to the internet,
@@ -47,7 +47,7 @@ fi
 
 echo ""
 echo "Retrieving aria2 script for the UUP set..."
-aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=b46a1232-2383-4626-946c-2d78dee5d557&pack=en-us&edition=ppipro&aria2=2"
+aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=2d652122-0ea3-477a-9807-c8506fec8fc4&pack=en-us&edition=ppipro&aria2=2"
 if [ $? != 0 ]; then
   echo "Failed to retrieve aria2 script"
   exit 1
@@ -71,5 +71,5 @@ fi
 echo ""
 if [ -e ./files/convert.sh ]; then
   chmod +x ./files/convert.sh
-  ./files/convert.sh esd "$destDir" 0
+  ./files/convert.sh wim "$destDir" 0
 fi
